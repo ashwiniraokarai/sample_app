3016ebb628de6784724ed22c0d36bc9be5a39ef4
Ubuntu <ubuntu@ip-172-31-44-68.us-east-2.compute.internal>
2021-01-19 07:07:46 +0000

    add static pages, controller tests, custom page titles using a helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def title_helper(page_title)
+    base_title = "Ruby on rails tutorial sample app"
+    full_title = page_title + " | " + base_title
+    if page_title.empty?
+      base_title
+    else
+      full_title
+    end
+  end
 end
