39bb133f8a0fefbde1b1455deffffc00d28f632c
ashwiniraokarai <ashwinirao.karai@gmail.com>
2021-02-11 07:49:20 +0000

    write test for title_helper method in application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  def title_helper(page_title)
+  def title_helper(page_title = "")
     base_title = "Ruby on rails tutorial sample app"
     full_title = page_title + " | " + base_title
     if page_title.empty?
