e45f79a04bcb2b7b47392ab25ca6f84bd3f83ce9
ashwiniraokarai <ashwinirao.karai@gmail.com>
2021-02-11 07:23:18 +0000

    validate layout links on homepage using integration tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   #define shorter, named routes
-  get "/home", to: "static_pages#home"
   get "/help", to: "static_pages#help"
   get "/about", to: "static_pages#about"
   get "/contact", to: "static_pages#contact"
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -5,12 +5,6 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   def setup
     @base_title =  "Ruby on rails tutorial sample app"
   end
-  
-  test "should get home" do
-    get home_path
-    assert_response :success
-    assert_select "title", "#{@base_title}"
-  end
 
   test "should get help" do
     get help_path
@@ -33,5 +27,6 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get root" do
     get root_path
     assert_response :success
+    assert_select "title", "#{@base_title}"
   end
 end
