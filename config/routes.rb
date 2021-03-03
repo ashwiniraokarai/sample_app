2d1f083f955c1e00e4f3e4b301ba8413d87e55c3
Ubuntu <ubuntu@ip-172-31-43-101.us-east-2.compute.internal>
2021-03-03 06:30:36 +0000

    add overall permanent debugger, and users resources

--- config/routes.rb
+++ config/routes.rb
@@ -15,4 +15,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # root "application#hello"
   root "static_pages#home"
+  resources :users
 end
