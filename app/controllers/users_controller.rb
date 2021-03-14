bfa3a98a331b134523f2811fdffce160e3e2fba0
Ubuntu <ubuntu@ip-172-31-43-101.us-east-2.compute.internal>
2021-03-14 01:07:40 +0000

    handle form submissions when there are errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,21 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+  
+  def create
+    # @user = User.new(params[:user])
+    @user = User.new(user_params)
+    if @user.save
+      #show page with the new user created
+    else
+      #show the same form but re-render with failed validations and all inputs pre-entered intact
+      render 'new'
+    end
+  end
+  
+  private
+    # to get around dangerous security flaw associated with params[:user] that mass assignment of attributes exposes, control which ones you will permit:
+    def user_params
+      params.require(:user).permit(:name, :emai, :password, :password_confirmation)
+    end
 end
