c27d24166afa544448d64372fbed9359bfc49213
Ubuntu <ubuntu@ip-172-31-43-101.us-east-2.compute.internal>
2021-03-16 02:18:42 +0000

    handle successful user save

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,12 +11,12 @@ class UsersController < ApplicationController
     # @user = User.new(params[:user])
     @user = User.new(user_params)
     if @user.save
-      #explicitly redirect to show page (vs the user#create view it looks for by default)
-      #you can do redirect_to @user for the "rails" way (less intuitive and blackboxish according to me)
+      #explicitly redirect to show page (or rails will look for the corresponding view for user#create which we don't create because pointless)
+      #you can do redirect_to @user instead for the "rails" way and let it infer (but less intuitive and blackboxish according to me)
       redirect_to user_path(@user)
     else
-      #show the same form (template) but re-render with failed validations and all inputs pre-entered intact
-      #the will show as /users in the url
+      #show the same form (template) again but re-render with failed validations and all inputs pre-entered intact
+      #this will show as /users in the url even though the template rendered is that of /users/new
       render 'new'
     end
   end
